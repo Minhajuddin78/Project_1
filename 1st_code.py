325ac2cf222c90bdda1a28aec444416defa58251
Minhajuddin78 <162782739+Minhajuddin78@users.noreply.github.com>
2024-03-09 12:22:10 +0530

    Add files via upload

---
+++ 1st_code.py
@@ -0,0 +1,9 @@
+b = int(input())
+if b > 30 :
+    print("yes b is greater than 30")
+elif b  > 40 : 
+    print("yes b is leser than 40 ")
+elif b > 100 : 
+    print("yes b is greater than 100")
+else :
+    print("this is my defalut")
